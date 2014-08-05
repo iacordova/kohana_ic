d271c3a5c1b78f621cff1ac3a17182a3eba7149b
Ivan Cordova <ivancordova@MacBook-Pro-de-Ivan.local>
2014-08-05 13:41:57 -0500

    Composer integration

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -132,6 +132,11 @@ Kohana::modules(array(
 	));
 
 /**
+ * Composer integration
+ */
+require APPPATH.'vendor/autoload.php';
+
+/**
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
